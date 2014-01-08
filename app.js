4d737d0e6d055cba7360f976aa38a91a82a4c234
Thomas Gebert <tombert@linux-4r4c.site>
2014-01-07 22:46:00 -0600

    Allow you to choose scope

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ module.exports = {
   callback: function(){
     //Convert to regular array to allow popping
     var args = Array.prototype.slice.call(arguments);
-    var myFunction = args[0]
+    var myScope = null; 
+    if(typeof args[0] == 'function'){
+      var myFunction = args[0]
+    }
+    else {
+      var myFunction = args[0].func
+      var myScope = args[0].scope
+    }
     args.shift()
     return function(cb){
       args.push(cb)
-      myFunction.apply(null, args)
+      myFunction.apply(myScope, args)
     }
   }
 }
